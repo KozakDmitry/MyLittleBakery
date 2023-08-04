90b9850b01c4a92a82a366839b5e14302fa51d69
Dmitry Kozak <dimakaza4ka@gmail.com>
2023-08-04 11:48:39 +0300

    Finished button for new pies

--- Assets/Scripts/ButtonRefresh.cs
+++ Assets/Scripts/ButtonRefresh.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,34 +8,41 @@ public class ButtonRefresh : MonoBehaviour
 {
     private Button PressButton;
     [SerializeField] private int timer;
-    [SerializeField] private GameObject block;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Slider unavailableSlider;
     private int localTimer;
     private Image image;
+    
 
     private void Start()
     {
         LevelManager.NewPieCreated += SetTimer;
         PressButton = GetComponent<Button>();
         image = GetComponent<Image>();
+        unavailableSlider.value = 0;
         image.enabled = false;
     }
 
     private void SetTimer()
     {
-
+       
         localTimer = timer;
-        block.transform.position = new Vector3(0, 0, 0);
+        text.text = localTimer.ToString();
+        text.gameObject.SetActive(true);
+        unavailableSlider.value = 1;
         InvokeRepeating(nameof(TickTimer), 1f,1f);
         image.enabled = true;
     }
     public void TickTimer()
     {
         localTimer--;
-        block.transform.position+= new Vector3(0, 0.1f, 0);
+        unavailableSlider.value -= 0.1f;
+        text.text = localTimer.ToString();
         if (localTimer <= 0)
         {
             image.enabled = false;
             CancelInvoke(nameof(TickTimer));
+            text.gameObject.SetActive(false);
         }
     }
 }
